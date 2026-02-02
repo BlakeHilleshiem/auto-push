2d8545ffabd9da0d888d7e64b9b614b992c3e5f6
Blake Hilleshiem <fosterbhilleshiem@gmail.com>
2026-02-02 08:54:49 -0700

    code cleanup

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
 import os
-import subprocess
-import schedule
 import time
 from datetime import datetime
 import csv
+
 from config import commit_days
-print(commit_days)
+
 days_order=['monday','tuesday','wednesday','thrusday','friday','saturday', 'sunday']
-day_indexes = [days_order.index(day) for day in commit_days]
-print(day_indexes)
+commit_day_indexes = [days_order.index(day) for day in commit_days]
 
-
-# def remind_commit():
-#     os.system('notify-send "Reminder: Push your code before dinner!"')
-
-# schedule.every().day.at("19:00").do(remind_commit)
-    # subprocess.Popen('echo Hello World!', shell=True)
-    # os.system('pwd')
-    # schedule.run_pending()
 now= datetime.now()
-day_idx = now.weekday()
 
-print(day_idx)
 while True:
     now_time_utc = datetime.now()
-    data={'message':'default msg', 'utc_date':now_time_utc}
-    with open('./history.csv', 'w', newline="") as csvfile:
-        fieldnames=['message', 'utc_date']
-        writer = csv.DictWriter(csvfile, fieldnames)
-        writer.writeheader()
-        writer.writerow(data)
-    
-    os.system('git add ./history.csv')
-    os.system(f'git commit -m "adds commit for {now_time_utc}"')
-    os.system('git push')
+    day_idx = now_time_utc.weekday()
+
+    if day_idx in commit_day_indexes:
+        data={'message':'default msg', 'utc_date':now_time_utc}
+        with open('./history.csv', 'a', newline="") as csvfile:
+            fieldnames=['message', 'utc_date']
+            writer = csv.DictWriter(csvfile, fieldnames)
+            writer.writeheader()
+            writer.writerow(data)
+        
+        os.system('git add ./history.csv')
+        os.system(f'git commit -m "adds commit for {now_time_utc}"')
+        os.system('git push')
+
     sleep_time = 60 * 60 * 24
     time.sleep(sleep_time)
